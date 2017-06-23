439e0da4d901f4ef2640676373ddf8d336e553ed
carrot <hexin_2006@126.com>
2017-06-23 16:29:40 +0800

    refactor functional test checking row text

--- function_tests.py
+++ function_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
 
         # Edith 听说有个很酷的在线待办事项应用
@@ -37,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后, 页面更新了
         # 待办事项表格中显示了 "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
         # 她输入了 "Use peacock feathers to make a fly"
@@ -50,11 +52,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith 想知道这个网站是否会记住她的清单
         # 他看到网站为她生成了一个唯一的 URL
