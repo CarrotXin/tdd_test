03eca7dd5f52bb95fdc88a9f7d3b6ae2b9696be5
carrot <hexin_2006@126.com>
2017-06-26 10:25:21 +0800

    Redirect after POST, and show all items in template

--- function_tests.py
+++ function_tests.py
@@ -1,7 +1,10 @@
 #-*- coding:utf-8 -*-
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -37,19 +40,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她在一个文本框内输入"Buy peacock feathers"
         # Edith 的爱好是使用假蝇做饵钓鱼
+        
         inputbox.send_keys('Buy peacock feathers')
 
         # 她按回车键后, 页面更新了
         # 待办事项表格中显示了 "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
         # 她输入了 "Use peacock feathers to make a fly"
         # Edith 做事很有条理
+        
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
         self.check_for_row_in_list_table('1: Buy peacock feathers')
