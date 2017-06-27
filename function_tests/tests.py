d73445d182d8987096477eacdc3165dbc549e236
carrot <hexin_2006@126.com>
2017-06-27 16:14:26 +0800

    update function tests

--- function_tests/tests.py
+++ function_tests/tests.py
@@ -47,6 +47,9 @@ class NewVisitorTest(LiveServerTestCase):
         # 待办事项表格中显示了 "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
@@ -63,14 +66,39 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Edith 想知道这个网站是否会记住她的清单
-        # 他看到网站为她生成了一个唯一的 URL
-        # 而且页面中有一些文字解说这个功能
+        # 现在一个叫做 Francis 的新用户访问了网站
+
+        ## 我们使用一个新的浏览器会话
+        ## 确保 Edith 的信息不回从 cookie 中泄漏出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis 访问首页
+        # 页面看不到 Edith 的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis 输入一个新待办事项，新建一个清单
+        # 他不像 Edith 那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Francis 获得了他的唯一 URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有 Edith 的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人很满意，去睡觉了
         self.fail("Finish the test!")
-
-        # 他去访问那个 URL，发现他的待办事项列表还在
-
-        # 他很满意，去睡觉了
 
 if __name__ == '__main__':
     unittest.main()
