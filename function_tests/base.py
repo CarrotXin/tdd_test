ebc80c4600208a96d23b8d0c43d6e9bc449e8bb7
carrot <hexin_2006@126.com>
2017-07-06 17:00:57 +0800

    use form in all views, back to working state

--- function_tests/base.py
+++ function_tests/base.py
@@ -1,8 +1,13 @@
 #-*- coding:utf-8 -*-
 import sys
+import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
+
+MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
     # in Django 1.11 removed manage.py test --liveserver option
@@ -27,6 +32,16 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
